28be74f79e3b9dbe80b70925a3e95330388e27f6
dbellol <dbellol@unal.edu.co>
2024-03-04 16:08:49 -0500

    Modificación user controller, authroute
    Login: user, admin, foundation

--- petadoptionBack/controller/userController.js
+++ petadoptionBack/controller/userController.js
@@ -47,7 +47,75 @@ const createUser = asyncHandler(async (req, res) => {
       throw new Error("Contraseña o usuario invalido");
     }
   });
+  /*Admin login*/
+  const loginAdmin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    //Verificar si un usuario existe o no
+    const findAdmin = await User.findOne({ email });
+    if (findAdmin.role !== "admin") throw new Error("No está autorizado");
+    if (findAdmin && (await findAdmin.isPasswordMatched(password))) {
+      const refreshToken = await generateRefreshToken(findAdmin?._id);
+      const updateuser = await User.findByIdAndUpdate(
+        findAdmin.id,
+        {
+          refreshToken: refreshToken,
+        },
+        {
+          new: true,
+        }
+      );
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 72 * 60 * 60 * 1000,
+      });
+      res.json({
+        _id: findAdmin?._id,
+        firstname: findAdmin?.firstname,
+        lastname: findAdmin?.lastname,
+        email: findAdmin?.email,
+        mobile: findAdmin?.mobile,
+        address: findAdmin?.address,
+        token: generateToken(findAdmin?._id),
+      });
+    } else {
+      throw new Error("Contraseña o usuario invalido");
+    }
+  });
+  /*Admin foundation*/
+  const loginFoundation = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    //Verificar si un usuario existe o no
+    const findFoundation = await User.findOne({ email });
+    if (findFoundation.role !== "foundation") throw new Error("No está autorizado");
+    if (findFoundation && (await findFoundation.isPasswordMatched(password))) {
+      const refreshToken = await generateRefreshToken(findFoundation?._id);
+      const updateuser = await User.findByIdAndUpdate(
+        findFoundation.id,
+        {
+          refreshToken: refreshToken,
+        },
+        {
+          new: true,
+        }
+      );
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 72 * 60 * 60 * 1000,
+      });
+      res.json({
+        _id: findFoundation?._id,
+        firstname: findFoundation?.firstname,
+        lastname: findFoundation?.lastname,
+        email: findFoundation?.email,
+        mobile: findFoundation?.mobile,
+        address: findFoundation?.address,
+        token: generateToken(findFoundation?._id),
+      });
+    } else {
+      throw new Error("Contraseña o usuario invalido");
+    }
+  });
   module.exports={
-    loginUserCtrl, createUser
+    loginUserCtrl, createUser, loginAdmin, loginFoundation
   };
   
--- petadoptionBack/routes/authRoute.js
+++ petadoptionBack/routes/authRoute.js
@@ -1,9 +1,12 @@
 const express=require('express');
-const {createUser,loginUserCtrl}=require('../controller/userController');
+const {createUser,loginUserCtrl, loginAdmin, loginFoundation}=require('../controller/userController');
 const router = express.Router();
 const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware");
 module.exports = router
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
+router.post("/admin-login", loginAdmin);
+router.post("/foundation-login", loginFoundation);
+
 module.exports=router;
